4dd24cc0485b14bdbcaf652d54bd307253b048c6
Joe Tam <joetam@spotify.com>
2016-10-12 01:29:30 -0400

    Consolidate dataSource.h into JTScrollableImageView.h

--- JTScrollableImageView/JTScrollableImageProvider.h
+++ JTScrollableImageView/JTScrollableImageProvider.h
@@ -1,5 +1,5 @@
 #import <Foundation/Foundation.h>
-#import "JTScrollableImageViewDataSource.h"
+#import "JTScrollableImageView.h"
 
 @interface JTScrollableImageProvider : NSObject <JTScrollableImageViewDataSource>
 
--- JTScrollableImageView/JTScrollableImageView.h
+++ JTScrollableImageView/JTScrollableImageView.h
@@ -1,5 +1,27 @@
 #import <UIKit/UIKit.h>
-#import "JTScrollableImageViewDataSource.h"
+
+typedef void (^ImageHandler)(UIImage *image);
+
+@protocol JTScrollableImageViewDataSource <NSObject>
+
+/**
+ *  Clients are responsible for calling 'next' and 'prev' when the UI has completed navigation to the next or the
+ *  previous image.
+ */
+- (void)next;
+- (void)prev;
+
+- (NSUInteger)currentIndex;
+- (void)currentImage:(ImageHandler)handler;
+- (void)prevImage:(ImageHandler)handler;
+- (void)nextImage:(ImageHandler)handler;
+
+@optional
+
+- (BOOL)hasNext;
+- (BOOL)hasPrev;
+
+@end
 
 @interface JTScrollableImageView : UIView
 
